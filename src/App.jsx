a868bec9326034844b4eae16c47d0398e2abed7f
Tetiana Mohorian <133663565+tetianamohorian@users.noreply.github.com>
2025-04-25 16:33:54 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import InfoBox from './components/InfoBox';
 import Footer from './components/Footer';
+import History from './components/History';
 
 import './App.css';
 
@@ -13,7 +14,8 @@ const App = () => {
   return (
     <div className="app-container">
       <Header />
-      <InfoBox /> 
+      <InfoBox />
+      <History />
       <Footer />
 
     </div>
