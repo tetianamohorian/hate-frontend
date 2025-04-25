d91c111a0839009a2cc239a47e5e2b5f4a3d322c
Tetiana Mohorian <133663565+tetianamohorian@users.noreply.github.com>
2025-04-25 15:28:26 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import InfoBox from './components/InfoBox';
 import Footer from './components/Footer';
