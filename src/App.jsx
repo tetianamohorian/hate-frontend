a41cf3a95ea74a788277aa940646823c2874e287
Tetiana Mohorian <133663565+tetianamohorian@users.noreply.github.com>
2025-04-25 16:43:43 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import InfoBox from './components/InfoBox';
-import History from './components/History';
+import History from './components/Historia';
 import Footer from './components/Footer';
 
 import './App.css';
