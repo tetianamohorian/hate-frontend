b9a854ca1f2492923e16f9110199f167c787b2a0
Tetiana Mohorian <133663565+tetianamohorian@users.noreply.github.com>
2025-04-27 17:44:22 +0200

    Update InfoBox.jsx

--- src/components/InfoBox.jsx
+++ src/components/InfoBox.jsx
@@ -6,7 +6,8 @@ const InfoBox = () => {
   const [headerText, setHeaderText] = useState('Analyzujte text na nenávistný jazyk');
   const [paragraphText, setParagraphText] = useState('Tento nástroj využíva umelú inteligenciu na identifikáciu toxického obsahu v textoch. Stačí zadať text a zistiť, či obsahuje nenávistný jazyk.');
   const [history, setHistory] = useState([]);
-
+  const [showHistory, setShowHistory] = useState(false);
+  
   const fetchHistory = async () => {
     try {
       const response = await fetch("https://hate-backend-production.up.railway.app/api/history");
@@ -41,11 +42,28 @@ const InfoBox = () => {
       
       <div className="flex flex-col items-center w-full">
         <ChatInput onSubmit={handleSendMessage} />
+        {!showHistory && (
+          <button
+            onClick={() => setShowHistory(true)}
+            className="bg-[#007AFF] hover:bg-[#005ECF] text-white font-semibold py-3 px-6 rounded-lg mt-6 transition"
+          >
+            🧾 Zobraziť históriu
+          </button>
+        )}
 
-        <h3 className="text-left text-white text-lg font-semibold mt-8 mb-4" style={{ backgroundColor: '#1A1A1A99',backdropFilter: 'blur(8px)'}}>🕘 História analýz</h3>
-        <div className="w-full mt-6 max-w-full mb-8"> 
-          <Historia history={history} />
-        </div>
+        
+        {showHistory && (
+          <>
+            <h3 className="text-left text-white text-lg font-semibold mt-8 mb-4"
+                style={{ backgroundColor: '#1A1A1A99', backdropFilter: 'blur(8px)', padding: '10px', borderRadius: '8px' }}>
+              🕘 História analýz
+            </h3>
+
+            <div className="w-full mt-6 max-w-full mb-8">
+              <Historia history={history} />
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
