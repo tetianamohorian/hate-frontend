48186215d4d0b1706844d43e8d6709b3d1316f6a
Tetiana Mohorian <133663565+tetianamohorian@users.noreply.github.com>
2025-04-28 21:56:08 +0200

    Update Historia.jsx

--- src/components/Historia.jsx
+++ src/components/Historia.jsx
@@ -7,27 +7,27 @@ const Historia = ({ history }) => {
 
   return (
     <div className="flex justify-center w-full">
-      <div className="overflow-x-auto w-[70%] min-w-[400px]">
-      <table className="table-fixed border-collapse text-gray-300 w-full">
-        <tbody>
-          {history.map((item, index) => (
-            <tr key={index} className={index % 2 === 0 ? 'bg-gray-800/40' : 'bg-gray-800/20'}>
-              <td className="px-6 py-3 text-left">{item.text}</td>
-              <td className="px-6 py-3 text-center">
-                <span className={`px-3 py-1 rounded-full text-xs font-medium ${
-                  item.prediction.includes("toxický") 
-                  ? "bg-red-500/40 text-red-100" 
-                  : "bg-green-500/40 text-green-100"
-                }`}>
-                  {item.prediction}
-                </span>
-              </td>
-              <td className="px-6 py-3 text-right text-gray-400 text-xs">{item.timestamp}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+      <div className="overflow-x-auto w-full max-w-3xl">
+        <table className="table-fixed border-collapse text-gray-300 w-full">
+          <tbody>
+            {history.map((item, index) => (
+              <tr key={index} className={index % 2 === 0 ? 'bg-gray-800/40' : 'bg-gray-800/20'}>
+                <td className="px-6 py-3 text-left">{item.text}</td>
+                <td className="px-6 py-3 text-center">
+                  <span className={`px-3 py-1 rounded-full text-xs font-medium ${
+                    item.prediction.includes("toxický") 
+                      ? "bg-red-500/40 text-red-100" 
+                      : "bg-green-500/40 text-green-100"
+                  }`}>
+                    {item.prediction}
+                  </span>
+                </td>
+                <td className="px-6 py-3 text-right text-gray-400 text-xs">{item.timestamp}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
